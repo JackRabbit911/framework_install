8226eac7a0712b5c217089dcfb175eeca1127cc2
alx <world.zay@gmail.com>
2024-03-01 13:52:49 +0300

    Blanks index and console was changed

--- folder/blanks/console.php
+++ folder/blanks/console.php
@@ -9,11 +9,12 @@ define('ROOTPATH', '{rootpath}');
 define('APPPATH', '{apppath}');
 define('SYSPATH', '{syspath}');
 define('WRITABLE', '{writable}');
+define('CONFIG', APPPATH . 'config/');
 
 require_once SYSPATH . 'vendor/autoload.php';
 require_once SYSPATH . 'vendor/az/sys/src/autoload.php';
 require_once SYSPATH . 'vendor/az/sys/src/library.php';
-require_once APPPATH . 'app/config/bootstrap.php';
+require_once CONFIG . 'bootstrap.php';
 
 $container = (new \Sys\ContainerFactory('cli'))
     ->create(new \DI\ContainerBuilder());
--- folder/blanks/index.php
+++ folder/blanks/index.php
@@ -11,6 +11,7 @@ define('ROOTPATH', '{rootpath}');
 define('SYSPATH', '{syspath}');
 define('APPPATH', '{apppath}');
 define('WRITABLE', '{writable}');
+define('CONFIG', APPPATH . 'config/');
 
 if (!empty(getenv('TZ'))) {
     date_default_timezone_set(getenv('TZ'));
@@ -19,35 +20,11 @@ if (!empty(getenv('TZ'))) {
 require_once SYSPATH . 'vendor/autoload.php';
 require_once SYSPATH . 'vendor/az/sys/src/autoload.php';
 require_once SYSPATH . 'vendor/az/sys/src/library.php';
-require_once APPPATH . 'app/config/bootstrap.php';
+require_once CONFIG . 'bootstrap.php';
 
-$mode = getMode(APPPATH . 'app/config/mode.php');
+$mode = getMode(CONFIG . 'mode.php');
 
 $container = (new Sys\ContainerFactory($mode))->create(new DI\ContainerBuilder());
 $app = $container->get(Sys\App::class);
 $app->run();
 FILE;
-
-// $GLOBALS['_start'] = hrtime(true);
-// $GLOBALS['_ram'] = memory_get_usage();
-
-// define('DOCROOT', __DIR__ . '/');
-// define('ROOTPATH', '../../');
-// define('SYSPATH', ROOTPATH . 'www/');
-// define('APPPATH', ROOTPATH . 'www/application/');
-// define('WRITABLE', ROOTPATH . 'www/writable/');
-
-// if (!empty(getenv('TZ'))) {
-//     date_default_timezone_set(getenv('TZ'));
-// }   
-
-// require_once SYSPATH . 'vendor/autoload.php';
-// require_once SYSPATH . 'vendor/az/sys/src/autoload.php';
-// require_once SYSPATH . 'vendor/az/sys/src/library.php';
-// require_once APPPATH . 'app/config/bootstrap.php';
-
-// $mode = getMode(APPPATH . 'app/config/mode.php');
-
-// $container = (new Sys\ContainerFactory($mode))->create(new DI\ContainerBuilder());
-// $app = $container->get(Sys\App::class);
-// $app->run();
