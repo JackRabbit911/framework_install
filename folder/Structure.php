f189259584e200eba3ef337dc9454a39134d94e5
alx <world.zay@gmail.com>
2024-03-02 14:35:48 +0300

    Wip. class Structure was created

---
+++ folder/Structure.php
@@ -0,0 +1,91 @@
+<?php
+
+class Structure
+{
+    private DrawFileSystem $draw;
+    // private string $projectName;
+    // private array $structure;
+
+    public function __construct()
+    {
+        $this->draw = new DrawFileSystem();
+        // $this->projectName = $project_name;
+        // $this->structure = $structure;
+    }
+
+    public function __invoke($structure)
+    {
+        echo 'Default structure:' . PHP_EOL;
+        echo $this->draw->draw($structure);
+               
+        return $this->prompt($structure);
+    }
+
+    private function prompt($structure)
+    {
+        echo PHP_EOL, 'Are You confirm? (Y/n) ';
+        $input = strtolower(trim(fgets(STDIN)));
+
+        $confirm = match ($input) {
+            'y', '' => $this->santize($structure),
+            'n' => $this->interactive($structure),
+            default => $this->prompt($structure)
+        };
+
+        return $confirm;
+    }
+
+    private function interactive($structure)
+    {
+        $structure['structure'] = $this->santize($structure['structure']);
+
+        $root = basename(getcwd());
+        $basepath = "$root/{$structure['projectName']}";
+
+        echo "Enter path to vendor, relative '$basepath/' folder: ";
+        $structure['structure']['syspath'] = ($this->inputHandle($basepath, 'vendor')) ?: '';
+        
+        echo "Enter path to app, relative '$basepath/' folder: ";
+        $structure['structure']['apppath'] = ($this->inputHandle($basepath, 'app')) ?: '';
+
+        echo "Enter path to document root, relative '$basepath/' folder: ";
+        $structure['structure']['docroot'] = ($this->inputHandle($basepath)) ?: '';
+
+        echo "Enter path to index.php, relative '{$structure['docroot']}/' folder: ";
+        $structure['structure']['entry_point'] = ($this->inputHandle($structure['structure']['docroot'], 'index.php')) ?: '';
+
+        $structure['structure']['writable'] = str_replace('application', 'writable', $structure['structure']['apppath']);
+        
+        echo $this->draw->draw($structure);
+
+        return $this->prompt($structure);
+    }
+
+    private function inputHandle($basepath, $end = '')
+    {
+        $input = trim(fgets(STDIN));
+        $input = trim($input, "\x22\x20\x27\x2F");
+
+        if (!empty($input)) {
+            $pos = strrpos($input, $end);
+            $input = ($pos) ? rtrim(substr_replace($input, '', $pos), '/') : $input;
+            return rtrim("$basepath/$input/$end", '/');
+        }
+
+        return str_replace('root/site.zone', $basepath, "root/site.zone/$end");
+    }
+
+    private function santize(&$structure)
+    {
+        
+        $root = basename(getcwd());
+        $replace = "$root/{$structure['project_name']}";
+
+        foreach ($structure['structure'] as &$path)
+        {
+            $path = str_replace('root/site.zone', $replace, $path);
+        }
+
+        return $structure;
+    }
+}
--- install.php
+++ install.php
@@ -8,53 +8,55 @@ new CheckRequirements;
 $handler = (new CheckArgument)($argv);
 $structure = $handler();
 
-echo PHP_EOL, 'Installation begins', PHP_EOL, PHP_EOL;
-$make = new MkAppSys($structure);
-$msg = $make->app();
-echo $msg, PHP_EOL;
+var_dump($structure);
 
-$msg = $make->vendor();
-echo $msg, PHP_EOL;
+// echo PHP_EOL, 'Installation begins', PHP_EOL, PHP_EOL;
+// $make = new MkAppSys($structure);
+// $msg = $make->app();
+// echo $msg, PHP_EOL;
 
-$mkDir = (new MkDir($structure));
+// $msg = $make->vendor();
+// echo $msg, PHP_EOL;
 
-$msg = $mkDir->writable();
-echo $msg, PHP_EOL;
+// $mkDir = (new MkDir($structure));
 
-$msg = $mkDir->docroot();
-echo $msg, PHP_EOL;
+// $msg = $mkDir->writable();
+// echo $msg, PHP_EOL;
 
-$mkFiles = new MkFiles($structure);
+// $msg = $mkDir->docroot();
+// echo $msg, PHP_EOL;
 
-$msg = $mkFiles->dockerFiles();
-echo $msg, PHP_EOL;
+// $mkFiles = new MkFiles($structure);
 
-$msg = $mkFiles->env();
-echo $msg, PHP_EOL;
+// $msg = $mkFiles->dockerFiles();
+// echo $msg, PHP_EOL;
 
-$msg = $mkFiles->console();
-echo $msg, PHP_EOL;
+// $msg = $mkFiles->env();
+// echo $msg, PHP_EOL;
 
-$msg = $mkFiles->index();
-echo $msg, PHP_EOL, PHP_EOL;
+// $msg = $mkFiles->console();
+// echo $msg, PHP_EOL;
 
-if (isset($structure['adminer']) && $structure['adminer'] === true) {
-    $paths = $structure['structure'];
-    $entry_point = dirname($paths['entry_point']) ?? $paths['docroot'];
-    $dir = str_replace([basename(getcwd()) . '/', '/index.php'], '', $entry_point);
-    $todb = $dir . '/todb';
+// $msg = $mkFiles->index();
+// echo $msg, PHP_EOL, PHP_EOL;
 
-    if (!is_dir($todb)) {
-        rename('folder/todb',  $dir . '/todb');
-    }
-}
+// if (isset($structure['adminer']) && $structure['adminer'] === true) {
+//     $paths = $structure['structure'];
+//     $entry_point = dirname($paths['entry_point']) ?? $paths['docroot'];
+//     $dir = str_replace([basename(getcwd()) . '/', '/index.php'], '', $entry_point);
+//     $todb = $dir . '/todb';
 
-if (!isset($structure['dev']['install']) || $structure['dev']['install'] === false) {
-    exec('rm -Rf folder');
-    exec('rm -Rf .git');
-    unlink('.gitignore');
-    unlink('install.php');
-}
+//     if (!is_dir($todb)) {
+//         rename('folder/todb',  $dir . '/todb');
+//     }
+// }
+
+// if (!isset($structure['dev']['install']) || $structure['dev']['install'] === false) {
+//     exec('rm -Rf folder');
+//     exec('rm -Rf .git');
+//     unlink('.gitignore');
+//     unlink('install.php');
+// }
 
 echo 'Insallation is complete!', PHP_EOL;
 echo 'Up Your docker container and go to localhost', PHP_EOL;
